6702100cb2d35bbe8d8ba505ab77b054ee95bfe6
z5548515ZhenhuanTan <tanzhenhuan818@outlook.com>
2024-07-29 23:00:24 +1000

    Add files via upload

---
+++ toolkit_config.py
@@ -0,0 +1,9 @@
+""" toolkit_config.py
+
+Project configuration file
+"""
+import os
+
+
+PRJDIR = '/Users/kk/Library/CloudStorage/OneDrive-UNSW/FINS5546/toolkit'
+DATADIR = os.path.join(PRJDIR, 'data')
